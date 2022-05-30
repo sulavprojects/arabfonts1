724378bee5f8eb4d37634c2f805cc212b2f99230
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-05-30 19:35:55 +0545

    first

--- arabfonts/settings/prod.py
+++ arabfonts/settings/prod.py
@@ -11,5 +11,5 @@ SECRET_KEY = 'django-insecure-(w4!+4bfxa5%)esio)egs0*ybll_lah4h1-z(4c5+g$vms00t@
 DEBUG = False
 # DEBUG = os.getenv("DEBUG", "False") == "True"
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ['arabfonts.herokuapp.com', '127.0.0.1' ]
+ALLOWED_HOSTS = ['arabicfonts.herokuapp.com', '127.0.0.1' ]
 # DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
